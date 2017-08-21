04d169c42bb53fa1c6ec33cbf0a579f09bacf610
Ravi Sravanam <ravi_sravanam@epam.com>
2017-08-21 16:27:12 +0530

    PS Script issue fixed

--- ApacheMesos/exec_cmd.ps1
+++ ApacheMesos/exec_cmd.ps1
@@ -4,17 +4,34 @@
 #docker tag ccc7a11d65b1 sravanam1242/private_docker:ubuntu-16.04
 #docker push sravanam1242/private_docker:ubuntu-16.04
 
-$image_repo = "sravanam1242/private_docker"
-$image_tag = "Apache-Mesos-POC-ubuntu-14.04"
-$docker_file_location = "."
-$docker_image_id = docker images -q ${image_repo}:${image_tag}
+#		DOCKER ARGUMETNS
+$IMAGE_REPO = "sravanam1242/private_docker"
+$IMAGE_TAG = "Apache-Mesos-POC-ubuntu-14.04"
+$DOCKER_FILE_LOCATION = "."
+$DOCKER_IMAGE_ID = docker images -q $IMAGE_REPO':'$IMAGE_TAG
 
-Write-Output $image_repo
-Write-Output $image_tag
-Write-Output $docker_file_location
-Write-Output $docker_image_id
+$MESSAGE = "**********CURRENT DOCKER IMAGE DETAILS**********"
 
-#Apache Mesos Master Installation (build with docker file)
-#docker build -t $image_repo:$image_tag $docker_file_location
-#docker commit $docker_image_id $image_repo:$image_tag
-#docker push $image_repo:$image_tag
+Write-Output $MESSAGE
+Write-Output ""
+Write-Output "IMAGE_REPO is $IMAGE_REPO"
+Write-Output "IMAGE_TAG is $IMAGE_TAG"
+Write-Output "DOCKER_FILE_LOCATION is $DOCKER_FILE_LOCATION"
+Write-Output "DOCKER_IMAGE_ID is $DOCKER_IMAGE_ID"
+Write-Output ""
+Write-Output "****************************************"
+
+#Apache Mesos Master Installation step (build with docker file)
+docker build -t $IMAGE_REPO':'$IMAGE_TAG $DOCKER_FILE_LOCATION
+docker commit $DOCKER_IMAGE_ID $IMAGE_REPO':'$IMAGE_TAG
+docker push $IMAGE_REPO':'$IMAGE_TAG
+
+$MESSAGE = "**********LATEST DOCKER IMAGE DETAILS**********"
+Write-Output $MESSAGE
+Write-Output ""
+Write-Output "IMAGE_REPO is $IMAGE_REPO"
+Write-Output "IMAGE_TAG is $IMAGE_TAG"
+Write-Output "DOCKER_FILE_LOCATION is $DOCKER_FILE_LOCATION"
+Write-Output "DOCKER_IMAGE_ID is $DOCKER_IMAGE_ID"
+Write-Output ""
+Write-Output "*********************************************"
